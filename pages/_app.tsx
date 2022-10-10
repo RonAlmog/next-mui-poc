58b7358bc4c63dc46345d1c54aeaa1a3ba175bb3
Ron Almog <ron8132@gmail.com>
2022-10-10 09:47:29 -0400

    use light theme, add document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Fragment, useEffect, useMemo, useState } from "react";
+// import Header from "../components/layout/header";
+import {
+  Badge,
+  Box,
+  createTheme,
+  CssBaseline,
+  Divider,
+  PaletteMode,
+  ThemeProvider,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { getStoredTheme, getThemeOptions, setStoredTheme } from "../src/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [mode, setMode] = useState<PaletteMode>("dark"); // default is dark mode
+  useEffect(() => {
+    const storedTheme = getStoredTheme();
+
+    if (storedTheme) {
+      setMode(storedTheme);
+    }
+  }, []);
+
+  // update the theme only if changed
+  const theme = useMemo(() => createTheme(getThemeOptions(mode)), [mode]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
